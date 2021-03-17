203fc11c3df485e2e0425b74d0d110259aa073e9
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-16 23:37:51 -0700

    completed

--- App.js
+++ App.js
@@ -2,51 +2,24 @@ import { useDimensions, useDeviceOrientation } from '@react-native-community/hoo
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, Alert, Button, Platform, Dimensions } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  console.log(require('./app/assets/icon.png'))
   return (
-    // styles.container is a plain JS object. Use {{}} for inline
-    // <SafeAreaView style={styles.container}>
-
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <StatusBar style="light" />
-      <Text style={{ color: 'white', fontSize: 35, letterSpacing: 5, fontFamily: 'Baskerville' }}>limited run</Text>
-      <Text>1234567</Text>
-      {/* <TouchableHighlight onPress={() => console.log('Image tapped')}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300" 
-          }}  
-        />
-      </TouchableHighlight> */}
-      <Button 
-        color="orange"
-        title="CLick Me" 
-          // onPress={() => Alert.alert("My Title", "My Message", [
-          //   {text: "Yes", onPress: () => console.log('Yes')},
-          //   {text: "No", onPress: () => console.log('No')},
-          // ])}
-        // Alert.prompt is iOS only
-        onPress={() => 
-          Alert.prompt("My Title", "My Message", text => console.log(text))
-        }
-      />
-    </SafeAreaView>
+    // <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
 const containerStyle = { backgroundColor: "#727272" };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    backgroundColor: '#727272',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: Platform.OS === 'android' ? 20 : 0,
-
-  },
+  // container: {
+  //   flex: 1, 
+  // },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
 });
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View, Text } from 'react-native';
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground 
-    style={styles.background}
+      style={styles.background}
       source={require('../assets/background.jpg')}
-      
-      >
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/logo-red.png')}
+        />
+        <Text>Sell things</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
     </ImageBackground>
   );
 }
@@ -15,8 +23,28 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-
-  }
+    justifyContent: "flex-end",
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
 })
 
 export default WelcomeScreen;
